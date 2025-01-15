1424135ae09e39d0500c786d1d4221684d05d4a4
Xylia-Rippy <xylia.rippy@gmail.com>
2025-01-15 15:07:44 -0800

    test

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+  let (:org) { Organization.new }
+
+  it "exists" do
+    Organization.new
+  end
+
+  it "responds to name" do
+    expect(org).to respond_to(:name)
+  end
+
+  it "responds to status" do
+    expect(org).to respond_to(:status)
+  end
+
+  it "has manty users" do
+    should have_mant(:users)
+  end
+
 end
