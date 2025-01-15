c6992f1358d8c5237967a132d81eafd1abc4d686
Xylia-Rippy <xylia.rippy@gmail.com>
2025-01-15 15:15:18 -0800

    KEEP MY SHIT TEHRE

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
+  
+  let (:org) { Organization.new }
+
+  it "exists" do
+    Organization.new
+  end
+
+  it "responds to name" do
+    expect(org).to respond_to(:name)
+  end
+
+  it "responds to status" do
+    expect(org).to respond_to(:status)
+  end
+
+  it "has many users" do
+    should have_many(:users)
+  end
+
 
 end
