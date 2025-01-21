01e01b41b169ec9c1be834d81fcf19edf893b90a
Xylia-Rippy <xylia.rippy@gmail.com>
2025-01-21 12:28:00 -0800

    added some more tests to organizations_spec.rb

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -16,8 +16,24 @@ RSpec.describe Organization, type: :model do
     expect(org).to respond_to(:status)
   end
 
+  it "responds to phone" do
+    expect(org).to respond_to(:phone)
+  end
+
+  it "responds to email" do
+    expect(org).to respond_to(:email)
+  end
+
+  it "responds to description" do
+    expect(org).to respond_to(:description)
+  end
+
   it "has many users" do
     should have_many(:users)
+  end
+
+  it "has many tickets" do
+    should have_many(:tickets)
   end
 
 
