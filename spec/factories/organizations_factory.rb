baba7e1be01ad0e7762c91341f4e9c71fc68f646
Austen Turbyne <austenturbyne@yahoo.com>
2025-02-06 14:58:08 -0800

    removed

--- spec/factories/organizations_factory.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-    factory :organization do
-      name { "organization1" }
-    end
-  end
-  
--- spec/factories/resource_category_factory.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-    factory :resource_category do
-      name { "resource1" }
-    end
-  end
