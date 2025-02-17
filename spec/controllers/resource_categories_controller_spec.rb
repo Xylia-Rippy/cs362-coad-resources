4037a71f1943169d5abfde288774af6a795c4d7e
Braxton <braxton.d.lowe@gmail.com>
2025-02-17 14:36:03 -0800

    resource contoller almost done, next is regions controller

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
 
+    describe 'as a logged out user' do
+        let(:user) { FactoryBot.create(:user) }
+    
+        it { expect(get(:index)).to redirect_to new_user_session_path }
+        it {
+          post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+          expect(response).to redirect_to new_user_session_path
+        }
+        it { expect(get(:new)).to redirect_to new_user_session_path }
+      end
+
 end
