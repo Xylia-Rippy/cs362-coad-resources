dc2d69b30d025a392a7f4fd8a87e05f60f934610
Braxton <braxton.d.lowe@gmail.com>
2025-02-24 15:07:29 -0800

    inclass challenge, and resource_cat controller done and editied

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,16 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
+  let(:admin) { create(:user, :admin) }
+  let(:user) { create(:user) }
+  let(:resource_category) { create(:resource_category) }
 
-    describe 'as a logged out user' do
-        let(:user) { FactoryBot.create(:user) }
-    
-        it { expect(get(:index)).to redirect_to new_user_session_path }
-        it {
-          post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
-          expect(response).to redirect_to new_user_session_path
-        }
-        it { expect(get(:new)).to redirect_to new_user_session_path }
+  describe 'GET #index' do
+    it 'redirects non-authenticated users' do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it 'allows authenticated users' do
+      sign_in admin
+      get :index
+      expect(response).to have_http_status(:ok)
+
+    end
+  end
+
+  describe 'POST #create' do
+    before { sign_in admin }
+
+    context 'with valid attributes' do
+      it 'creates a new category' do
+        expect {
+          post :create, params: { resource_category: { name: 'New Category' } }
+        }.to change(ResourceCategory, :count).by(1)
       end
+    end
 
+    context 'with invalid attributes' do
+      it 'does not create a category' do
+        expect {
+          post :create, params: { resource_category: { name: '' } }
+        }.not_to change(ResourceCategory, :count)
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    before { sign_in admin }
+
+    it 'updates the category' do
+      patch :update, params: { id: resource_category.id, resource_category: { name: 'Updated Name' } }
+      expect(resource_category.reload.name).to eq('Updated Name')
+    end
+  end
+
+  describe 'PATCH #activate' do
+    before { sign_in admin }
+
+    it 'activates the category' do
+      allow_any_instance_of(ResourceCategory).to receive(:activate).and_return(true)
+      patch :activate, params: { id: resource_category.id }
+      expect(response).to redirect_to(resource_category)
+      expect(flash[:notice]).to eq('Category activated.')
+    end
+  end
+
+  describe 'PATCH #deactivate' do
+    before { sign_in admin }
+
+    it 'deactivates the category' do
+      allow_any_instance_of(ResourceCategory).to receive(:deactivate).and_return(true)
+      patch :deactivate, params: { id: resource_category.id }
+      expect(response).to redirect_to(resource_category)
+      expect(flash[:notice]).to eq('Category deactivated.')
+    end
+  end
+
+  # describe 'DELETE #destroy' do
+  #   before { sign_in admin }
+
+  #   it 'deletes the category' do
+  #     resource_category
+  #     expect {
+  #       delete :destroy, params: { id: resource_category.id }
+  #     }.to change(ResourceCategory, :count).by(-1)
+  #   end
+  # end
 end
--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
 
+    before do
+        @user = create(:user, :admin)
+    end
+
+    it 'can be used' do
+
+        log_in_as @user
+        visit regions_path
+
+        click_on 'Add Region'
+    
+        fill_in 'Name', with: 'Test Name'
+    
+        click_on 'Add Region'
+    
+        expect(current_path).to eq regions_path
+        expect(page.body).to have_text('Test Name')
+
+    end
+
+
 end
