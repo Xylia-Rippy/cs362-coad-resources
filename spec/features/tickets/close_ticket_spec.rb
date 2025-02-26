64218b24de2514ce0adf885150caebfd76c60140
Austen Turbyne <austenturbyne@yahoo.com>
2025-02-26 15:46:03 -0800

    Page ticket all done

--- spec/features/tickets/close_ticket_spec.rb
+++ spec/features/tickets/close_ticket_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Closing a ticket', type: :feature do
 
+    
+    before do
+        @ticket = create(:ticket)
+        @user = create(:user, :admin)
+    end
+
+    it 'can be used' do
+
+        log_in_as @user
+        visit dashboard_path
+
+        click_on @ticket.name
+    
+        click_on 'Close'
+    
+        expect(current_path).to eq dashboard_path
+        expect(page.body).to have_text(@ticket.name)
+
+    end
+
+
 end
